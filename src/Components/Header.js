8fb637879d51f65ae55e596b468a72552309117b
swasti101 <swastij64@gmail.com>
2024-06-01 16:00:42 +0530

    updated img elements in (header and meme).js files

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,11 @@ import React from "react";
 export default function Header() {
   return (
     <header className="header">
-      <img className="header--img" src="./images/Troll Face.png" />
+      <img
+        className="header--img"
+        alt="image_here"
+        src="./images/Troll Face.png"
+      />
       <h4 className="header--title">Meme Generator</h4>
       <p className="header--proj">React Course - project 4</p>
     </header>
--- src/Components/Meme.js
+++ src/Components/Meme.js
@@ -81,7 +81,7 @@ export default function Meme() {
         </button>
       </div>
       <div className="meme">
-        <img src={meme.randomImage} className="meme--image" />
+        <img src={meme.randomImage} alt="image_here" className="meme--image" />
         <h2 className="meme--text top">{meme.topText}</h2>
         <h2 className="meme--text bottom">{meme.bottomText}</h2>
       </div>
